1ab8a804dc21c9f4bf4e8cdf8097c04f6b76836a
knishina <knishina@yahoo-corp.jp>
2021-02-19 01:40:03 +0900

    Automation test practiced

---
+++ selenium-sample/automation.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+chrome_browser = webdriver.Chrome('./chromedriver')
+
+# chrome_browser.maximize_window()
+chrome_browser.get(
+    'https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+assert 'Selenium Easy' in chrome_browser.title
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+
+# print(show_message_button.get_attribute('innerHTML'))
+
+assert'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+user_message.clear()
+user_message.send_keys('this is automatically typed')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'this is automatically typed' in output_message.text
+
+chrome_browser.quit()
